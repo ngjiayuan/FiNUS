94eca4c8991b59f0b13aefec3fc606a7d818ec67
jackgugz <jackgugz@users.noreply.github.com>
2021-05-26 15:04:41 +0800

    addingpage isExpense status added

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { AddingPage, HeaderButton } from "./src/features/AddingPage";
+import { AddingPage } from "./src/features/AddingPage";
 import { MainPage } from "./src/features/MainPage";
 
 function MainScreen({ navigation }) {
   return <MainPage onButtonPress={() => navigation.navigate("AddingPage")} />;
 }
 
-function AddingScreen() {
-  return <AddingPage />;
+function AddingScreen({ navigation }) {
+  return <AddingPage navigation={navigation} />;
 }
 
 const Stack = createStackNavigator();
@@ -20,13 +20,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="MainPage">
         <Stack.Screen name="MainPage" component={MainScreen} />
-        <Stack.Screen
-          name="AddingPage"
-          component={AddingScreen}
-          options={{
-            headerTitle: (props) => <HeaderButton />,
-          }}
-        />
+        <Stack.Screen name="AddingPage" component={AddingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
